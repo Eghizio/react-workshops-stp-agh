2571465953c4ceb98d497629f2ef78fe2df4aafd
jakub-wasik <jakub.wasik@schibsted.com>
2023-11-20 22:53:40 +0100

    Separate data file

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,5 @@
 import { TechnologyEnjoyers } from "./components/TechnologyEnjoyers/TechnologyEnjoyers";
-import { Student } from "./model";
-
-const students: Student[] = [
-  { name: "Kuba", technology: "React" },
-  { name: "Piotrek", technology: "React" },
-  { name: "Tomasz", technology: "Angular" },
-  { name: "Stefan", technology: "Angular" },
-  { name: "Ada", technology: "React" },
-  { name: "Magda", technology: "React" },
-  { name: "Zuza", technology: "Angular" },
-];
+import { students } from "./data";
 
 export const App = () => (
   <main>
---
+++ src/data.ts
@@ -0,0 +1,11 @@
+import { Student } from "./model";
+
+export const students: Student[] = [
+  { name: "Kuba", technology: "React" },
+  { name: "Piotrek", technology: "React" },
+  { name: "Tomasz", technology: "Angular" },
+  { name: "Stefan", technology: "Angular" },
+  { name: "Ada", technology: "React" },
+  { name: "Magda", technology: "React" },
+  { name: "Zuza", technology: "Angular" },
+];
